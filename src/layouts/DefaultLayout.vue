0e60de54b0ba96885b4a309907f0f79491960c3a
sawich <sawich@angeldev.org>
2023-04-09 08:56:09 +0300

    🎨 style(main.postcss): change background color of html to zinc-900 🔥 chore(main.postcss): remove cursor-default from global styles 💄 style(DefaultLayout.vue): add padding to RouterView and section, add TheSidebar component to DefaultLayout.vue

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+
+import TheSidebar from "@/components/TheSidebar.vue";
+</script>
+
+<template>
+  <section class="grid grid-cols-[auto_1fr] gap-2">
+    <TheSidebar />
+    <RouterView class="py-4 pl-2 pr-4" />
+  </section>
+</template>
