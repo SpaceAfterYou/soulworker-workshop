872d603d75b05825a8aeb157d0341d55c11cc44d
sawich <sawich@angeldev.org>
2023-04-09 08:51:24 +0300

    🚀 chore(Cargo.toml): update project name, version, and repository URL 🔒 chore(Cargo.toml): update dependencies: rust-version to 1.68.2, add reqwest and zip 🔧 chore(Cargo.toml): reformat Cargo.toml file
    🚀 feat(main.rs): add `on_install_plugin` command to handle plugin installation with error handling
    
    🚀 chore(tauri.conf.json): update package name and version to "SoulWorker Workshop" and "25.0.0" respectively
    🔒 chore(tauri.conf.json): add fs to allowlist with all permissions and scope set to all files and directories
    🎨 chore(tauri.conf.json): update window title to "SoulWorker Workshop" and set width to 1024

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,41 +3,94 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::api::shell;
-use tauri::{CustomMenuItem, Manager, Menu, Submenu};
+use serde::Serialize;
+use std::{io::Cursor, ops::Not, path::Path, vec};
+use tauri::Manager;
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum CommandError {
+    UrlCantDownload,
+    UrlCantGetBuffer,
+    PathNotExists,
+    PathIsNotDir,
+    ZipCantExtract,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CommandErrorResult {
+    status: bool,
+    code: CommandError,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CommandInstallPluginResult {
+    status: bool,
+    files: Vec<String>,
+}
+
+pub type CommandResult<T> = Result<T, CommandErrorResult>;
 
 #[tauri::command]
-fn backend_add(number: i32) -> i32 {
-    // Note: these commands block the main thread and hang the UI until they return.
-    // If you need to run a long-running task, use async command instead.
-    println!("Backend was called with an argument: {}", number);
-    number + 2
+async fn on_install_plugin(url: String, dir: String) -> CommandResult<CommandInstallPluginResult> {
+    let response = reqwest::get(url).await;
+    if response.is_err() {
+        return Err(CommandErrorResult {
+            code: CommandError::UrlCantDownload,
+            status: false,
+        });
+    }
+
+    let buffer = response.unwrap().bytes().await;
+    if buffer.is_err() {
+        return Err(CommandErrorResult {
+            code: CommandError::UrlCantGetBuffer,
+            status: false,
+        });
+    }
+
+    let path = Path::new(&dir);
+    if path.exists().not() {
+        return Err(CommandErrorResult {
+            code: CommandError::PathNotExists,
+            status: false,
+        });
+    }
+
+    if path.is_dir().not() {
+        return Err(CommandErrorResult {
+            code: CommandError::PathIsNotDir,
+            status: false,
+        });
+    }
+
+    let cursor = Cursor::new(buffer.unwrap());
+
+    let mut a = zip::ZipArchive::new(cursor).unwrap();
+
+    if a.extract(path).is_err() {
+        return Err(CommandErrorResult {
+            code: CommandError::ZipCantExtract,
+            status: false,
+        });
+    }
+
+    return Ok(CommandInstallPluginResult {
+        files: a
+            .file_names()
+            .filter(|p| p.ends_with('/').not())
+            .map(|v| v.to_string())
+            .collect(),
+        status: true,
+    });
 }
 
 fn main() {
     let ctx = tauri::generate_context!();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![backend_add])
-        .menu(
-            tauri::Menu::os_default("Tauri Vue Template").add_submenu(Submenu::new(
-                "Help",
-                Menu::with_items([CustomMenuItem::new(
-                    "Online Documentation",
-                    "Online Documentation",
-                )
-                .into()]),
-            )),
-        )
-        .on_menu_event(|event| {
-            let event_name = event.menu_item_id();
-            match event_name {
-                "Online Documentation" => {
-                    let url = "https://github.com/Uninen/tauri-vue-template".to_string();
-                    shell::open(&event.window().shell_scope(), url, None).unwrap();
-                }
-                _ => {}
-            }
-        })
+        .invoke_handler(tauri::generate_handler![on_install_plugin])
         .setup(|_app| {
             #[cfg(debug_assertions)]
             {
